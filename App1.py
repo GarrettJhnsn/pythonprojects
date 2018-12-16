9a69964458f5ae463b4443628134a21d8afd9f98
Garrettjhnsn <garrettjhns0n8@gmail.com>
2018-12-16 11:14:35 -0800

    App1 Completed - Simple age calculator for the future

---
+++ App1.py
@@ -0,0 +1,9 @@
+print("#### Simple age calculator ####")
+print("#### for the year 2025     ####\n")
+
+born = input("Enter Birth Year: ")
+born = int(born)
+age = 2025 - born
+
+print("In the year 2025 you will be", age, "years old!")
+
