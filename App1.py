0e9f5329dc28e083d119d0b96f94a32539855b97
Garrettjhnsn <garrettjhns0n8@gmail.com>
2018-12-16 11:17:21 -0800

    Added Comments

--- App1.py
+++ App1.py
@@ -1,5 +1,11 @@
+#Title For Application
+
 print("#### Simple age calculator ####")
 print("#### for the year 2025     ####\n")
+
+#Input with with value converter and simple equation.
+#Change calculator year by adjusting age variable x 2018-2XXX
+
 
 born = input("Enter Birth Year: ")
 born = int(born)
