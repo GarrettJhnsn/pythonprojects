4f929058da9e981445deb58fb905b9ed28b2e51a
Garrettjhnsn <garrettjhns0n8@gmail.com>
2018-12-16 11:23:37 -0800

    Created Simple Calculator to determine a dogs age

---
+++ DogAgeCalculator1.py
@@ -0,0 +1,8 @@
+print("#### Simple age converter for dogs ####\n")
+
+
+dog_human_age =(input("What is your dogs age in human years?: "))
+dog_human_age = int(dog_human_age)
+dog_age = 7 * dog_human_age
+
+print("Your dog is", dog_age, "years old in dog years!")
