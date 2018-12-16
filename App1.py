004f83dd132819f1737d76d4a79ab9c72f307442
GarrettJhnsn <45772450+GarrettJhnsn@users.noreply.github.com>
2018-12-16 11:18:14 -0800

    Update App1.py

--- App1.py
+++ App1.py
@@ -4,7 +4,7 @@ print("#### Simple age calculator ####")
 print("#### for the year 2025     ####\n")
 
 #Input with with value converter and simple equation.
-#Change calculator year by adjusting age variable x 2018-2XXX
+#Change calculator year by adjusting age variable x [2018-2xxx]
 
 
 born = input("Enter Birth Year: ")
