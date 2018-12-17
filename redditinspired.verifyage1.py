cfc796915e168e02afc55629edb256132089bb01
Garrettjhnsn <garrettjhns0n8@gmail.com>
2018-12-16 18:39:41 -0800

    CLEANED CODE/ADDED COMMENTS/FINISHED

--- redditinspired.verifyage1.py
+++ redditinspired.verifyage1.py
@@ -1,38 +1,37 @@
-## 
-##  THIS DETERMINES SOMEONE AGE FOR THE DAY IT WAS CREATED
-##  IT ONLY VERIFYS PEOPLE WHO TURNED 18 EITHER ON OR BEFORE DEC, 16 2018
-##  VARIABLES CAN BE ADJUSTED TO CURRENT DAY
-##  NO AUTOMATIC ADJUSTER JUST BASIC PROGRAMMING
-
-
 print("####              ****Please use only whole numbers****             ####")
 print("####    If you enter anything else you will get an error            ####")
 print("####    I hope you enjoy this small demo of determining             ####")
 print("####    someones eligibility to enter a site based on there age!    ####\n")
 
+"""USER INPUT/NUMBERS ONLY/NO FLOAT"""
 
-
-user_day = input("Please enter the day you were born [In Numbers]: ")
+user_day = input("Please enter the day you were born [In Numbers}: ")
 user_month = input("Please enter the month you were born [In Numbers]: ")
 user_year = input("Please enter the year you were born [In Numbers]:")
 
-min_agereq_day = int(16)
-min_agereq_month = int(12)
-min_agereq_year = int(2000)
+"""CONVERSION FROM STRING TO INT/IF NOT CONVERSION IF STATEMENT FAILS"""
 
-iuser_day = int(user_day)
-iuser_month = int(user_month)
-iuser_year = int(user_year)
+min_age_req_day = int(16)
+min_age_req_month = int(12)
+min_age_req_year = int(2000)
 
+"""USER INPUT INT CONVERTED VARIABLES"""
 
-if iuser_day <= min_agereq_day and iuser_month <= min_agereq_month and iuser_year <= min_agereq_year:
+i_user_day = int(user_day)
+i_user_month = int(user_month)
+i_user_year = int(user_year)
+
+"""IF YOU MEET THE MIN REQ STATED IN MIN_AGE_REQ VALUE PRINTS WELCOME/IF NOT MEETING MIN REQ PRINTS ELSE"""
+
+if i_user_day <= min_age_req_day and i_user_month <= min_age_req_month and i_user_year <= min_age_req_year:
     print("You are old enough to enter the site!")
 else:
-    print("You are not old enough to enter the site!\n")
+    print("You are not old enough to enter the site!")
 
 
+"""THANK YOU"""
+
+print("\n")
 print("Thanks for checking out my demo!")
 print("Created by Garrett Johnson,2018")
 
-
-## Conversion to integer from string variables.
