6623455fb96f7bd850c950b08cf59d44d9beb72c
Garrettjhnsn <garrettjhns0n8@gmail.com>
2018-12-16 18:22:17 -0800

    Cleaned Code Up

--- random_numberguess1.py
+++ random_numberguess1.py
@@ -1,6 +1,6 @@
 from random import randint
 
-## total_guess holds the value of how many attempts have been made to guess the number
+#total_guess holds the value of how many attempts have been made to guess the number
 
 total_guess = []
 
@@ -10,21 +10,21 @@ print("#### Pick any number between 1 and 5 and see if  ####")
 print("#### you can guess the random number!            ####")
 print("#### You will also only receive 3 guesses!       ####\n")
 
-##user_guess is the variable for the input string and it is converted into an integer on the following line.
+#user_guess is the variable for the input string and it is converted into an integer on the following line.
 
 
-user_guess= input("Enter a number between 1 and 5 \n")
+user_guess = input("Enter a number between 1 and 5 \n")
 guess_int = int(user_guess)
 total_guess.append(user_guess)
 
-##random_num is a variable that holds the value of the randomly generated number
-##So (1, 5) = 1, 2, 3, 4, 5 allowing a number to be randomly selected from the range
+#random_num is a variable that holds the value of the randomly generated number
+#So (1, 5) = 1, 2, 3, 4, 5 allowing a number to be randomly selected from the range
 
 random_num = randint(1, 5)
 
 print("Your guess:", user_guess)
 
-##While len(total_guess) < 3: allows for the loop to continue for only 3 guesses
+#While len(total_guess) < 3: allows for the loop to continue for only 3 guesses
 
 while len(total_guess) < 3:
     if guess_int != random_num:
@@ -33,12 +33,11 @@ while len(total_guess) < 3:
         guess_int = int(user_guess)
         print("Your guess:", user_guess)
 
-##The loop will continue until total_guess > 2 or guess_int == random_num
-
+#The loop will continue until total_guess > 2 or guess_int == random_num
+print("The random number was:", random_num)
 if guess_int == random_num:
     print("Congrats! You guessed the correct number!")
 
-print("The random number was:", random_num)
 print("\n")
 print("Thanks for playing!")
 print("Created by Garrett Johnson,2018")
